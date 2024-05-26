4348ea51fed1bf2222aff0ed97c35af347021c12
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-05-26 03:39:56 +0000

    feat(requirements-storage): upload to google drive

--- api/post-lead/post.go
+++ api/post-lead/post.go
@@ -1,17 +1,23 @@
 package post
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/googleapi"
+	"google.golang.org/api/option"
 )
 
 var validate *validator.Validate
+var driveService *drive.Service
 
 func init() {
 	validate = validator.New(validator.WithRequiredStructEnabled())
@@ -30,7 +36,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		log.Printf("[PostLead] Error: %s", err)
+		log.Printf("[PostLead] Bad request \"%s\"", err)
 
 		response := make(map[string]string)
 		response["error"] = err.Error()
@@ -54,8 +60,9 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 			errs[i] = fmt.Sprintf("%s has invalid value '%s': %s", err.Namespace(), err.Value(), err.Error())
 		}
 
-		response := make(map[string][]string)
-		response["error"] = errs
+		response := map[string][]string{
+			"error": errs,
+		}
 
 		jsonResponse, _ := json.Marshal(response)
 
@@ -66,7 +73,51 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	body.uuid = uuid.NewString()
 
 	// TODO: Process files
+	files := []*os.File{}
+
 	// TODO: Send to GoogleDrive
+	uploadedFiles := []*drive.File{}
+	if len(files) > 0 {
+		if driveService == nil {
+			driveService = createGoogleDriveService()
+		}
+
+		for _, file := range files {
+			stat, _ := file.Stat()
+
+			fileMetadata := &drive.File{
+				Name: file.Name(),
+			}
+
+			res, err := driveService.Files.
+				Create(fileMetadata).Media(file, googleapi.ChunkSize(int(stat.Size()))).
+				Do()
+
+			if err != nil {
+				log.Printf("[PostLead] Error \"%s\" while processing enquiry for \"%s\"", err, body.Email)
+
+				response := make(map[string]string)
+				response["error"] = err.Error()
+
+				jsonResponse, _ := json.Marshal(response)
+
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write(jsonResponse)
+			}
+
+			uploadedFiles = append(uploadedFiles, res)
+		}
+	}
+
 	// TODO: POST to CRM
 	// TODO: Send email
+}
+
+func createGoogleDriveService() *drive.Service {
+	service, err := drive.NewService(context.Background(), option.WithCredentialsFile(os.Getenv("GCLOUD_CREDENTIALS")))
+	if err != nil {
+		log.Fatalf("[PostLead] Unabled to access Drive API %s", err)
+	}
+
+	return service
 }
