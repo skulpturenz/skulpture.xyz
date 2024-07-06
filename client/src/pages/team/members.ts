2c06f1b60afd947d2648401361d308205ce83425
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-07-06 10:43:56 +0000

    feat: about and team page

---
+++ client/src/pages/team/members.ts
@@ -0,0 +1,25 @@
+export const TEAM = [
+	{
+		firstName: "Thomas",
+		lastName: "Mathew",
+		image: "/team/thomas.jpg",
+		cv: "/team/thomas.pdf",
+		profile:
+			"Thomas brings a wealth of experience in project management and operations having worked for 30 years in construction on large public infrastructure projects. Thomas read civil engineering at Memphis State University, Tennessee back in 1987.",
+	},
+	{
+		firstName: "Naveen",
+		lastName: "Mathew",
+		image: "/team/naveen.jpg",
+		cv: "/team/naveen.pdf", // TODO
+		profile:
+			"Naveen has been working professionally with technology for 3 years. Originally from Malaysia he moved to New Zealand in 2018 to study a BSc in Physics at the University of Otago.",
+	},
+	{
+		firstName: "Santiago",
+		lastName: "Villiesid",
+		image: "/team/santiago.jpg",
+		cv: "/team/santiago.pdf",
+		profile: "", // TODO
+	},
+];
