e559bc54bd6659835fdb9c8ccb0525b58f40bfde
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-08-03 11:08:23 +0000

    chore: update deps

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -2,7 +2,6 @@ import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 import path from "path";
 import { fileURLToPath } from "url";
-import eslintPluginAstro from "eslint-plugin-astro";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
@@ -20,7 +19,6 @@ const compat = new FlatCompat({
 export default [
 	js.configs.recommended,
 	...tseslint.configs.recommended,
-	...eslintPluginAstro.configs.recommended,
 	...compat.env({
 		browser: true,
 		es2020: true,
