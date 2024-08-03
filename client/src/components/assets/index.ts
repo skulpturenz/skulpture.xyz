2d195bf5e7f01562e3883937eb920be322d52c0f
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-08-03 22:30:52 +0000

    chore: add more assets, figma file and block images

--- client/src/components/assets/index.ts
+++ client/src/components/assets/index.ts
@@ -1,3 +1,7 @@
 /// <reference types="vite-plugin-svgr/client" />
+export * from "./icons";
 export { default as LogoDark } from "./logo-dark.svg?react";
 export { default as LogoLight } from "./logo-light.svg?react";
+export { default as LogoIconDark } from "./logo-icon-dark.svg?react";
+export { default as LogoIconLight } from "./logo-icon-light.svg?react";
+export { default as LogoWords } from "./logo-words.svg?react";
