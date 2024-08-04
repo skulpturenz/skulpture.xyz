de53befae733ba2764a44ef530a9333b94323668
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-08-04 01:57:16 +0000

    feat: base layout

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -10,21 +10,22 @@ const buttonVariants = cva(
 		variants: {
 			variant: {
 				default:
-					"bg-primary text-primary-foreground shadow hover:bg-primary/90",
+					"font-semibold bg-primary text-primary-foreground shadow hover:bg-primary/90",
 				destructive:
-					"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+					"font-semibold bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
 				outline:
-					"border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+					"font-semibold border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
 				secondary:
-					"bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-				ghost: "hover:bg-accent hover:text-accent-foreground",
-				link: "text-primary underline-offset-4 hover:underline",
+					"font-semibold bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
+				ghost: "font-semibold hover:bg-accent hover:text-accent-foreground",
+				link: "text-foreground font-semibold underline-offset-4 hover:underline",
 			},
 			size: {
-				default: "h-9 px-4 py-2",
-				sm: "h-8 rounded-md px-3 text-xs",
-				lg: "h-10 rounded-md px-8",
+				default: "rounded-full h-9 px-5 py-5",
+				sm: "h-8 rounded-full px-3 text-xs",
+				lg: "h-10 rounded-full px-8",
 				icon: "h-9 w-9",
+				inherit: "",
 			},
 		},
 		defaultVariants: {
