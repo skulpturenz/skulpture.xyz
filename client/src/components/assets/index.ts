3f2dda0ee33e185cd7701d1ffdc1c36f6b862426
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-08-07 10:50:54 +0000

    feat: add bg pattern for intro section

--- client/src/components/assets/index.ts
+++ client/src/components/assets/index.ts
@@ -5,3 +5,5 @@ export { default as LogoLight } from "./logo-light.svg?react";
 export { default as LogoIconDark } from "./logo-icon-dark.svg?react";
 export { default as LogoIconLight } from "./logo-icon-light.svg?react";
 export { default as LogoWords } from "./logo-words.svg?react";
+export { default as topographyDark } from "./topography-dark.svg";
+export { default as topographyLight } from "./topography-light.svg";
--- client/src/components/constants.ts
+++ client/src/components/constants.ts
@@ -1,4 +1,7 @@
 export const constants = {
 	contentContainer: "mx-auto max-w-7xl px-4 sm:px-6 lg:px-8",
 	pageContainer: "min-h-screen max-w-screen py-20",
+	id: {
+		landingIntro: "landing-intro",
+	},
 };
