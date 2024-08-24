3a90294764557547d236f8d1fef52ba3cb344342
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-08-24 11:21:31 +0000

    test: tests for field validations

--- api/main_test.go
+++ api/main_test.go
@@ -46,6 +46,239 @@ func TestCreateEnquiry(t *testing.T) {
 	assert.Equal(t, http.StatusCreated, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
 }
 
+func TestValidateFirstNameRequired(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"lastName": strings.NewReader("123"),
+		"email":    strings.NewReader("test@test.com"),
+		"mobile":   strings.NewReader("+64123412342"),
+		"enquiry":  strings.NewReader("Hello world"),
+		"files":    file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateLastNameRequired(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"email":     strings.NewReader("test@test.com"),
+		"mobile":    strings.NewReader("+64123412342"),
+		"enquiry":   strings.NewReader("Hello world"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateEmailRequired(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"lastName":  strings.NewReader("123"),
+		"mobile":    strings.NewReader("+64123412342"),
+		"enquiry":   strings.NewReader("Hello world"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateEmailFormat(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"lastName":  strings.NewReader("123"),
+		"email":     strings.NewReader("test"),
+		"mobile":    strings.NewReader("+64123412342"),
+		"enquiry":   strings.NewReader("Hello world"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateMobileOptional(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"lastName":  strings.NewReader("123"),
+		"email":     strings.NewReader("test@example.com"),
+		"enquiry":   strings.NewReader("Hello world"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusCreated, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateMobileFormat(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"lastName":  strings.NewReader("123"),
+		"email":     strings.NewReader("test"),
+		"mobile":    strings.NewReader("12345678"),
+		"enquiry":   strings.NewReader("Hello world"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
+func TestValidateEnquiryRequired(t *testing.T) {
+	file, err := os.CreateTemp(os.TempDir(), "create_enquiry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file.Name())
+
+	enquiry := map[string]io.Reader{
+		"firstName": strings.NewReader("Test"),
+		"lastName":  strings.NewReader("123"),
+		"email":     strings.NewReader("test"),
+		"mobile":    strings.NewReader("12345678"),
+		"files":     file,
+	}
+
+	contentType, form, err := createMultipartForm(enquiry)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res, err := http.Post("http://localhost:80/contact", contentType, form)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusBadRequest, res.StatusCode, strings.Join([]string{res.Status, string(body)}, "\n"))
+}
+
 // From: https://stackoverflow.com/a/20397167
 func createMultipartForm(values map[string]io.Reader) (string, *bytes.Buffer, error) {
 	var b bytes.Buffer
